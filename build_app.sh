7fa91f4efd0d8a2fa0676071b2725ce5a633df55
amari <amari.lawal@gmail.com>
2024-02-05 17:54:22 +0000

    hello

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
-git push origin 
+git push origin -u main:main
 docker build -t palondomus/caesaraigcp:latest .
 docker push palondomus/caesaraigcp:latest
 docker run -it -p 8080:8080 palondomus/caesaraigcp:latest
