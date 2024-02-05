898278923a5101298658f59cb6be2314e895fc28
amari <amari.lawal@gmail.com>
2024-02-05 18:22:03 +0000

    GCPStreamUpload works

--- CaesarAIGCP/CaesarAIGCPStreamUpload.py
+++ CaesarAIGCP/CaesarAIGCPStreamUpload.py
@@ -74,3 +74,9 @@ class CaesarAIGCPStreamUpload(CaesarAIGCP):
 
     def tell(self) -> int:
         return self._read
+if __name__ == "__main__":
+    #client = storage.Client()
+
+    with CaesarAIGCPStreamUpload(bucket_name="caesaraiyoutube-bucket", blob_name='test-blob2') as s:
+        for _ in range(1024):
+            s.write(b'x' * 1024)
--- CaesarAIGCP/rungcp.py
+++
@@ -1,7 +0,0 @@
-from CaesarAIGCPStreamUpload import CaesarAIGCPStreamUpload
-if __name__ == "__main__":
-    #client = storage.Client()
-
-    with CaesarAIGCPStreamUpload(bucket_name="caesaraiyoutube-bucket", blob_name='test-blob2') as s:
-        for _ in range(1024):
-            s.write(b'x' * 1024)
